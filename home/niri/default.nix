7d1c83b96b90b290298c80a04f151326ceb8f60e
TheItroma <edmondliseuse@gmail.com>
2025-12-29 22:32:47 -0500

    ah

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -4,6 +4,9 @@
   xdg.configFile."niri/config.kdl".source = ./config.kdl;
 
   home.packages = with pkgs; [
+    hyprpolkitagent # Just a polkit brah
+    xdg-desktop-portal-gtk # Basic functionality
+    xdg-desktop-portal-gnome # Screencast support
     xwayland-satellite # Xwayland support
   ];
 }
