08b32018b7210bbb6d2881d4b2c9616ac8b26776
TheItroma <edmondliseuse@gmail.com>
2026-03-11 22:25:46 -0400

    ah

--- modules/hosts/hosts.nix
+++ modules/hosts/hosts.nix
@@ -1,80 +1,94 @@
-{ inputs, lib, config, self, ... }:
+{ inputs, lib, config, self, ... }@flakeArgs:
 
 let
-  inherit (lib) types mkOption mapAttrs elemAt optionalAttrs;
-  inherit (builtins) null;
+  inherit (lib) types mkOption mapAttrs;
 
-in {
-  options =
-    let
-      baseHostModule = { config, name, ... }: {
-        options = {
-          system = mkOption { type = types.str; default = "x86_64-linux"; };
-          modules = mkOption { type = with types; listOf deferredModule; default = [ ]; };
-          primaryUser = mkOption { type = types.str; default = "itroma"; };
-          specialArgs = mkOption { type = with types; attrsOf anything; default = { }; };
-          nixpkgs = mkOption { type = types.pathInStore; };
-          pkgs = mkOption { type = types.pkgs; };
-        };
-        config = {
-          nixpkgs = inputs.nixpkgs;
-          pkgs = import config.nixpkgs { inherit (config) system; config.allowUnfree = true; };
-          specialArgs = { inherit inputs; inherit (config) primaryUser; };
-        };
-      };
-
-      hostTypeNixos = types.submodule [
-        baseHostModule
-        ({ name, config, inputs, self, ... }: {
-          options.homeManagerModules = lib.mkOption {
-            type = with lib.types; listOf deferredModule;
-            default = [ ];
-          };
-          config.modules = [
-            ({ primaryUser, ... }: {
-              home-manager.users.${primaryUser}.imports = config.homeManagerModules;
-            })
-            ({ self, config, primaryUser, inputs, ... }: {
-              imports = [ inputs.home-manager.nixosModules.home-manager ];
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-
-                users.${primaryUser}.imports = [
-                  self.modules.homeManager.core
-                  { home.homeDirectory = config.users.users.${primaryUser}.home; }
-                ];
-
-                extraSpecialArgs = {
-                  inherit (self) inputs;
-                  inherit primaryUser;
-                  configName = "nixos_${config.networking.hostName}";
-                  nhSwitchCommand = "nh os switch";
-                };
-              };
-            })
-          ];
-        })
-        ({ name, self, ... }: {
-          modules = [
-            self.modules.nixos.core
-            { networking.hostName = name; }
-            (self.modules.nixos."host_${name}" or { })
-          ];
-        })
-      ];
-
-    in {
-      nixosHosts = mkOption { type = types.attrsOf hostTypeNixos; };
+  # Base host module template
+  baseHostModule = { config, name, ... }: {
+    options = {
+      system = mkOption { type = types.str; default = "x86_64-linux"; };
+      modules = mkOption { type = with types; listOf lib.deferredModule; default = [ ]; };
+      primaryUser = mkOption { type = types.str; default = "itroma"; };
+      specialArgs = mkOption { type = with types; attrsOf lib.anything; default = { }; };
+      nixpkgs = mkOption { type = types.pathInStore; };
+      pkgs = mkOption { type = types.pkgs; };
     };
 
-  config.flake = {
-    nixosConfigurations =
-      let
-        mkHost = hostname: options:
-          options.nixpkgs.lib.nixosSystem {
-            inherit (options) system modules specialArgs;
+    config = {
+      nixpkgs = inputs.nixpkgs;
+      pkgs = import config.nixpkgs {
+        inherit (config) system;
+        config.allowUnfree = true;
+      };
+      specialArgs = {
+        inherit inputs;
+        inherit (config) primaryUser;
+      };
+    };
+  };
+
+  # Host type for NixOS submodules
+  hostTypeNixos = types.submodule [
+    baseHostModule
+
+    # Home Manager + host-specific configuration
+    ({ name, config, inputs, self, ... }: {
+      options.homeManagerModules = mkOption {
+        type = with types; listOf lib.deferredModule;
+        default = [ ];
+      };
+
+      config.modules = [
+
+        # Apply host-specific Home Manager modules
+        ({ primaryUser, ... }: {
+          home-manager.users.${primaryUser}.imports = config.homeManagerModules;
+        })
+
+        # Home Manager core + extra special args
+        ({ config, primaryUser, inputs, self, ... }: {
+          imports = [ inputs.home-manager.nixosModules.home-manager ];
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+
+            users.${primaryUser}.imports = [
+              self.modules.homeManager.core
+              { home.homeDirectory = config.users.users.${primaryUser}.home; }
+            ];
+
+            extraSpecialArgs = {
+              inherit (self) inputs;
+              inherit primaryUser;
+              configName = "nixos_${config.networking.hostName}";
+              nhSwitchCommand = "nh os switch";
+            };
           };
-      in mapAttrs mkHost config.nixosHosts;
+        })
+      ];
+    })
+
+    # Host-specific NixOS modules
+    ({ name, self, ... }: {
+      modules = [
+        self.modules.nixos.core
+        { networking.hostName = name; }
+        (self.modules.nixos."host_${name}" or { })
+      ];
+    })
+  ];
+
+in {
+  options = {
+    nixosHosts = mkOption { type = types.attrsOf hostTypeNixos; };
+  };
+
+  config.flake = {
+    nixosConfigurations = let
+      mkHost = hostname: options:
+        options.nixpkgs.lib.nixosSystem {
+          inherit (options) system modules specialArgs;
+        };
+    in mapAttrs mkHost config.nixosHosts;
   };
 }
