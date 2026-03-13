5681dd70dadf9e90889e2dec0b05fffb7a151dc9
TheItroma <edmondliseuse@gmail.com>
2026-03-13 19:21:25 -0400

    aH

--- modules/home/core/home-manager.nix
+++ modules/home/core/home-manager.nix
@@ -1,6 +1,5 @@
 {
   flake.modules.homeManager.home-manager = {
-    home.stateVersion = "25.05";
     programs.home-manager.enable = true;
   };
 }
--- modules/hosts/poppySeed/_nixos/default.nix
+++ modules/hosts/poppySeed/_nixos/default.nix
@@ -7,4 +7,5 @@
   time.hardwareClockInLocalTime = true;
 
   system.stateVersion = "25.11";
+  home-manager.home.stateVersion = "25.05";
 }
